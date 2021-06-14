02779b58dab9a8dd3543199973c11dd00258a603
SafaanHashmi <55549354+SafaanHashmi@users.noreply.github.com>
2021-06-14 18:26:36 +0530

    Update functions.inc.php

--- functions.inc.php
+++ functions.inc.php
@@ -12,14 +12,7 @@
 		die();
 	}
 
-	function get_safe_value($con,$str)
-	{
-		if($str!='')
-		{
-			$str=trim($str);
-			return mysqli_real_escape_string($con,$str);
-		}
-	}
+	
 
 
     function get_product($con,$limit='',$cat_id='',$product_id='')
